b276f8d9ab73c2d85e612774bc0d375a21322d26
Sergey Nazarov <natarajaya@gmail.com>
2019-01-30 16:36:08 -0800

    Add helm-tls module

---
+++ modules/helm-tls/variables.tf
@@ -0,0 +1,18 @@
+variable "secrets_dir" {}
+
+# This module will generate TLS assets that may later be used
+# To secure Tiller installation by helm-initializer module with TLS authentication.
+# By default, TLS assets will be generated to a following directory:
+# ${TF_VAR_secrets_dir}/${custom_tls_dir:-tiller-namespace}/${helm_dir_name}
+
+variable "custom_tls_dir" {
+  default = ""
+}
+
+variable "helm_dir_name" {
+  default = "helm-tls"
+}
+
+variable "tiller_namespace" {
+  default = "kube-system"
+}
