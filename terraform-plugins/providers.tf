4c0e6470bc122a23abe6b921468190d5e65fd840
Stepan Stipl <stepan@stipl.net>
2019-05-23 17:40:21 +0100

    GPII-3940: Upgrade terrafrom and providers

--- terraform-plugins/providers.tf
+++ terraform-plugins/providers.tf
@@ -1,41 +1,45 @@
 # This is the list of Terraform plugins to be installed in the exekube container
 
 provider "google" {
-  version = "~> 2.1.0"
+  version = "~> 2.7.0"
 }
 
 provider "google-beta" {
-  version = "~> 2.1.0"
+  version = "~> 2.7.0"
 }
 
 provider "random" {
-  version = "~> 2.0.0"
+  version = "~> 2.1.2"
 }
 
 provider "null" {
-  version = "~> 1.0.0"
+  version = "~> 2.1.2"
 }
 
 provider "kubernetes" {
-  version = "~> 1.4.0"
+  version = "~> 1.6.2"
 }
 
 provider "template" {
-  version = "~> 1.0.0"
+  version = "~> 2.1.2"
 }
 
 provider "tls" {
-  version = "~> 1.2.0"
+  version = "~> 2.0.1"
 }
 
 provider "local" {
-  version = "~> 1.1.0"
+  version = "~> 1.2.2"
 }
 
 provider "external" {
-  version = "~> 1.0.0"
+  version = "~> 1.1.2"
 }
 
 provider "aws" {
-  version = "~> 1.52.0"
+  version = "~> 2.11.0"
+}
+
+provider "helm" {
+  version = "~> 0.9.1"
 }
