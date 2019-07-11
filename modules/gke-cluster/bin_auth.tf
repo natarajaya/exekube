b18e231b29da80da38a5b8b2825ece5f4ae23ac4
Tyler Roscoe <tyler@cryptio.net>
2019-07-10 21:57:06 -0600

    Add dedicated binauth SA workaround (thanks Stepan).

--- modules/gke-cluster/bin_auth.tf
+++ modules/gke-cluster/bin_auth.tf
@@ -1,6 +1,22 @@
-resource "google_binary_authorization_policy" "policy" {
+# Since we sometimes use ADCs, and since the binaryauthorization API does not
+# allow ADCs, we need a dedicated SA to manage binary auth. See GPII-3860.
+data "google_service_account_access_token" "bin-auth" {
   count    = "${var.enable_binary_authorization ? 1 : 0}"
   provider = "google-beta"
+
+  target_service_account = "gke-cluster-bin-auth@${var.project_id}.iam.gserviceaccount.com"
+  scopes                 = ["cloud-platform"]
+  lifetime               = "300s"
+}
+
+provider "google-beta" {
+  alias        = "bin-auth"
+  access_token = "${data.google_service_account_access_token.bin-auth.access_token}"
+}
+
+resource "google_binary_authorization_policy" "policy" {
+  count    = "${var.enable_binary_authorization ? 1 : 0}"
+  provider = "google-beta.bin-auth"
   project  = "${var.project_id}"
 
   default_admission_rule {
